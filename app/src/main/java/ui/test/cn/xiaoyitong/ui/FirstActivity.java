0d765befe1ae31196444551fa8ddced9533873cd
zhangyilan <2521653792@qq.com>
2017-05-15 21:11:56 +0800

    张艺蓝

--- app/src/main/java/ui/test/cn/xiaoyitong/ui/FirstActivity.java
+++ app/src/main/java/ui/test/cn/xiaoyitong/ui/FirstActivity.java
@@ -1,11 +1,16 @@
 package ui.test.cn.xiaoyitong.ui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import ui.test.cn.xiaoyitong.R;
 
@@ -18,6 +23,10 @@ public class FirstActivity extends TestActivity {
     private Button btn_register;
     private Button btn_login;
 
+    ImageView mImageView1;
+    ImageView mImageView2;
+    ImageView mImageView3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +37,8 @@ public class FirstActivity extends TestActivity {
         setContentView(R.layout.activity_first);
         btn_register = (Button) findViewById(R.id.btn_register);
         btn_login = (Button) findViewById(R.id.btn_login);
-
+        initViews();
+        startAnimation();
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,5 +55,55 @@ public class FirstActivity extends TestActivity {
                 finish();
             }
         });
+    }
+
+
+    private void initViews() {
+        mImageView1 = (ImageView) findViewById(R.id.image1);
+        mImageView2 = (ImageView) findViewById(R.id.image2);
+        mImageView3 = (ImageView) findViewById(R.id.image3);
+    }
+
+
+    private void startAnimation() {
+        ObjectAnimator anim1 = new ObjectAnimator().ofFloat(mImageView1, "alpha", 1f, 0f).setDuration(5000);
+        ObjectAnimator anim2 = new ObjectAnimator().ofFloat(mImageView2, "alpha", 0f, 1f).setDuration(5000);
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(anim1, anim2);
+
+        ObjectAnimator anim3 = new ObjectAnimator().ofFloat(mImageView2, "alpha", 1f, 0f).setDuration(5000);
+        ObjectAnimator anim4 = new ObjectAnimator().ofFloat(mImageView3, "alpha", 0f, 1f).setDuration(5000);
+        AnimatorSet set1 = new AnimatorSet();
+        set1.playTogether(anim3, anim4);
+
+        ObjectAnimator anim5 = new ObjectAnimator().ofFloat(mImageView3, "alpha", 1f, 0f).setDuration(5000);
+        ObjectAnimator anim6 = new ObjectAnimator().ofFloat(mImageView1, "alpha", 0f, 1f).setDuration(5000);
+        AnimatorSet set2 = new AnimatorSet();
+        set2.playTogether(anim5, anim6);
+
+        AnimatorSet set3 = new AnimatorSet();
+        set3.playSequentially(set, set1, set2);
+        set3.addListener(new AnimatorListenerAdapter() {
+
+            private boolean mCanceled;
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mCanceled = false;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCanceled = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (!mCanceled) {
+                    animation.start();
+                }
+            }
+        });
+        set3.start();
     }
 }
